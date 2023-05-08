afb1118c998605731c59c76d90f2daf7dbc553c4
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-08 19:15:32 +1000

    feat: Add conditional rendering for filtered restaurants and display a message when no results are found

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,19 @@ function App() {
       <h1>Restaurant Recommender</h1>
       <RestaurantFilter restaurants={restaurantsData} onFilterChange={handleFilterChange} />
       <h2>Filtered Restaurants</h2>
-      {filteredRestaurants.map((restaurant) => (
-        <div key={restaurant.name}>
-          <h3>{restaurant.name}</h3>
-          <p>Price Bracket: {restaurant.priceBracket}</p>
-          <p>Delivery Time: {restaurant.deliveryTimeMinutes} mins</p>
-          <p>Open Hours: {restaurant.openHour} - {restaurant.closeHour}</p>
-          <p>Distance: {restaurant.distance} km</p>
-        </div>
-      ))}
+      {filteredRestaurants.length > 0 ? (
+        filteredRestaurants.map((restaurant) => (
+          <div key={restaurant.name}>
+            <h3>{restaurant.name}</h3>
+            <p>Price Bracket: {restaurant.priceBracket}</p>
+            <p>Delivery Time: {restaurant.deliveryTimeMinutes} mins</p>
+            <p>Open Hours: {restaurant.openHour} - {restaurant.closeHour}</p>
+            <p>Distance: {restaurant.distance} km</p>
+          </div>
+        ))
+      ) : (
+        <p>Please try another option to find restaurants.</p>
+      )}
     </div>
   );
 }
